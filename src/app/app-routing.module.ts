74767ce3fe33d96d169a8093c943d53c63f04f32
LucLev <luckylevillain@gmail.com>
2019-06-05 16:10:39 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { NewInfoComponent } from './new-info/new-info.component';
+import { StatsComponent } from './stats/stats.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'info', component: NewInfoComponent },
+  { path: 'stats', component: StatsComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewInfoComponent } from './new-info/new-info.component';
+import { StatsComponent } from './stats/stats.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewInfoComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
