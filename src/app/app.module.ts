e9c3bafd7309d94d64a08462cecb2c57375def9f
yorannelisabeth <yorannelisabeth@gmail.com>
2019-06-07 11:25:14 +0200

    conflict resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
-=======
+
+
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +11,6 @@ import { HomeComponent } from './home/home.component';
 import { NewInfoComponent } from './new-info/new-info.component';
 import { StatsComponent } from './stats/stats.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { MyServiceService } from './my-service.service';
 import { FormulaireComponent } from './formulaire/formulaire.component';
 import { HeaderComponent } from './header/header.component';
